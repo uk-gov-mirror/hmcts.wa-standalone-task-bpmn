59fcc7e559b875dbf5ccb7b6821829d861571403
lewis150193 <lewis.williams@hmcts.net>
2020-11-02 16:11:15 +0000

    added tests and bpm files

--- src/main/java/uk/gov/hmcts/reform/wa/standalone/task/bpmn/Application.java
+++ src/main/java/uk/gov/hmcts/reform/wa/standalone/task/bpmn/Application.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.demo;
+package uk.gov.hmcts.reform.wa.standalone.task.bpmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
