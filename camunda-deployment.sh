57ebd601557cdbead4515f7ddee656a305add7c6
lewis150193 <lewis.williams@hmcts.net>
2020-12-15 14:07:10 +0000

    add auto deplyoment

--- camunda-deployment.sh
+++ camunda-deployment.sh
@@ -8,19 +8,9 @@
 
 AUTHORIZATION= sh ${WA_KUBE_ENV_PATH}/scripts/actions/idam-service-token.sh
 
+BASEDIR=$(dirname "$0")
 
-for file in ${WA_BPMNS_DMNS_PATH}/*.bpmn ${WA_BPMNS_DMNS_PATH}/*.dmn
-do
-	if [ -f "$file" ]
-	then
-curl --header "Content-Type: multipart/form-data" "ServiceAuthorization: ${AUTHORIZATION}"\
-  --request POST \
-  --form data=@$file \
-  "http://camunda-bpm/engine-rest/deployment/create"
-  fi
-done
-
-for file in ${IA_TASK_DMNS_BPMNS_PATH}/*.bpmn ${IA_TASK_DMNS_BPMNS_PATH}/*.dmn
+for file in $BASEDIR/src/main/resources/*.bpmn $BASEDIR/src/main/resources/*.dmn
 do
 	if [ -f "$file" ]
 	then
