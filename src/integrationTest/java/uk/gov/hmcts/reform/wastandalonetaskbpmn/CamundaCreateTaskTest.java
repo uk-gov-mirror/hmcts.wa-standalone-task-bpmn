c3ce0f44613ee455f44128d667aa82a72873c1bc
Vamshidhar Chitti <indvamshi@gmail.com>
2021-01-15 10:20:05 +0000

    Rwa 246 delay until (#40)
    * Added DelayUntil timer to the task. Updated tests
    
    * updated tests
    
    * checkstyle fix
    
    * adding name to terminated event

--- src/integrationTest/java/uk/gov/hmcts/reform/wastandalonetaskbpmn/CamundaCreateTaskTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wastandalonetaskbpmn/CamundaCreateTaskTest.java
@@ -1,27 +1,29 @@
 package uk.gov.hmcts.reform.wastandalonetaskbpmn;
 
+import org.camunda.bpm.engine.ManagementService;
+import org.camunda.bpm.engine.runtime.ActivityInstance;
+import org.camunda.bpm.engine.runtime.JobQuery;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.time.LocalDate;
-import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 import static java.time.ZonedDateTime.now;
 import static java.time.format.DateTimeFormatter.ISO_INSTANT;
 import static java.util.Date.from;
 import static java.util.Map.of;
-import static org.apache.commons.lang3.time.DateUtils.isSameDay;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.assertThat;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.complete;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.task;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.job;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
 import static org.junit.Assert.assertTrue;
 import static uk.gov.hmcts.reform.wastandalonetaskbpmn.ProcessEngineBuilder.getProcessEngine;
 
@@ -38,15 +40,43 @@ public class CamundaCreateTaskTest {
     @Rule
     public ProcessEngineRule processEngineRule = new ProcessEngineRule(getProcessEngine());
 
+    private ManagementService managementService;
+
+    @Before
+    public void setUp() {
+        managementService = processEngineRule.getManagementService();
+    }
+
     @Test
     @Deployment(resources = {"wa-task-initiation-ia-asylum.bpmn"})
-    public void createsAndCompletesATaskWithADueDate() {
+    public void createsAndCompletesATaskWithDelayUntilTimer() {
         ProcessInstance processInstance = startCreateTaskProcess(of(
             "taskId", "provideRespondentEvidence",
             "group", EXPECTED_GROUP,
             "dueDate", DUE_DATE_STRING,
-            "name", TASK_NAME
+            "name", TASK_NAME,
+            "delayUntil", ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
         ));
+
+        ActivityInstance activityInstance = runtimeService().getActivityInstance(processInstance.getId());
+        ActivityInstance[] childActivityInstances = activityInstance.getChildActivityInstances();
+
+        // only timer activity is created
+        assertTrue(childActivityInstances.length == 1);
+        assertTrue(childActivityInstances[0].getActivityType().equals("intermediateTimer"));
+        assertThat(processInstance).isStarted()
+            .task().isNull();
+        BpmnAwareTests.assertThat(processInstance).isWaitingAt("processStartTimer");
+
+        JobQuery jobQuery = managementService.createJobQuery().processInstanceId(processInstance.getId());
+        // will execute the delayUtil timer manually
+        managementService.executeJob(jobQuery.singleResult().getId());
+
+        activityInstance = runtimeService().getActivityInstance(processInstance.getId());
+        childActivityInstances = activityInstance.getChildActivityInstances();
+
+        assertTrue(childActivityInstances.length == 1);
+        assertTrue(childActivityInstances[0].getActivityId().equals(PROCESS_TASK));
 
         assertThat(processInstance).isStarted()
             .task()
@@ -56,43 +86,37 @@ public class CamundaCreateTaskTest {
             .hasName(TASK_NAME)
             .isNotAssigned();
         assertThat(processInstance)
-            .job()
-            .hasDueDate(DUE_DATE_DATE);
+            .job();
         complete(task(PROCESS_TASK));
         assertThat(processInstance).isEnded();
     }
 
     @Test
     @Deployment(resources = {"wa-task-initiation-ia-asylum.bpmn"})
-    public void createsAndCompletesATaskWithoutADueDate() {
-        Map<String, Object> processVariables = new HashMap<>();
-        processVariables.put("taskId", "provideRespondentEvidence");
-        processVariables.put("group", EXPECTED_GROUP);
-        processVariables.put("dueDate", null);
-        processVariables.put("name", TASK_NAME);
-        ProcessInstance processInstance = startCreateTaskProcess(processVariables);
+    public void createsAndWaitsForDelayUntilTimerIsTriggered() {
+        String testBusinessKey = "TestBusinessKey";
+        ProcessInstance processInstance = startCreateTaskProcessWithBusinessKey(of(
+            "taskId", "provideRespondentEvidence",
+            "group", EXPECTED_GROUP,
+            "dueDate", DUE_DATE_STRING,
+            "name", TASK_NAME,
+            "delayUntil", ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+        ), testBusinessKey);
 
+        ActivityInstance activityInstance = runtimeService().getActivityInstance(processInstance.getId());
+        ActivityInstance[] childActivityInstances = activityInstance.getChildActivityInstances();
+
+        // only timer activity is created
+        assertTrue(childActivityInstances.length == 1);
+        assertTrue(childActivityInstances[0].getActivityType().equals("intermediateTimer"));
         assertThat(processInstance).isStarted()
-            .task()
-            .hasDefinitionKey(PROCESS_TASK)
-            .hasCandidateGroup(EXPECTED_GROUP)
-            .hasName(TASK_NAME)
-            .isNotAssigned();
+            .task().isNull();
 
-        Date dueDate = task().getDueDate();
-        Date expectedDueDate = from(LocalDate.now().plusDays(2).atStartOfDay().toInstant(ZoneOffset.UTC));
-        assertTrue(
-            "Expected [" + dueDate + "] to be on [" + expectedDueDate + "]",
-            isSameDay(dueDate, expectedDueDate)
-        );
+        BpmnAwareTests.assertThat(processInstance).isWaitingAt("processStartTimer");
 
-        Date jobDueDate = job().getDuedate();
-        assertTrue(
-            "Expected [" + jobDueDate + "] to be on [" + expectedDueDate + "]",
-            isSameDay(jobDueDate, expectedDueDate)
-        );
-        complete(task(PROCESS_TASK));
-        assertThat(processInstance).isEnded();
+
+        processEngineRule.getRuntimeService().correlateMessage("cancelTasks", testBusinessKey);
+        BpmnAwareTests.assertThat(processInstance).isEnded();
     }
 
     @Test
@@ -103,8 +127,12 @@ public class CamundaCreateTaskTest {
         ProcessInstance createTaskAndCancel = startCreateTaskProcessWithBusinessKey(of(
             "group", EXPECTED_GROUP,
             "dueDate", DUE_DATE_STRING,
-            "name", TASK_NAME
+            "name", TASK_NAME,
+            "delayUntil", ZonedDateTime.now().plusSeconds(1).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
         ), testBusinessKey);
+
+        JobQuery jobQuery = managementService.createJobQuery().processInstanceId(createTaskAndCancel.getId());
+        managementService.executeJob(jobQuery.singleResult().getId());
 
         assertThat(createTaskAndCancel).isStarted()
             .task()
